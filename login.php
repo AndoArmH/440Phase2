df32bff55f11b5b80de999cac39bc7cf98a96b8a
= <=>
2023-04-16 00:36:27 -0700

    Init Database

--- login.php
+++ login.php
@@ -5,7 +5,7 @@ include("connection.php");
 include("functions.php");
 
 //check to see if user clicked sign up button
-if ($_SERVER['REQUEST_METHOD'] == "POST") {
+if ($_SERVER['REQUEST_METHOD'] == "POST" && isset($_POST['username'])) {
 	//something was posted
 	$username = $_POST['username'];
 	$password = $_POST['password'];
@@ -36,7 +36,91 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
 	} else {
 		echo "wrong username or password!";
 	}
+} else 
+
+if ($_SERVER['REQUEST_METHOD'] == "POST") {
+	$users_json = file_get_contents('init.json');
+	$decoded_json = json_decode($users_json, true);
+	$users = $decoded_json['users'];
+	$items = $decoded_json['items'];
+	$reviews = $decoded_json['reviews'];
+
+	mysqli_query($con, "DROP TABLE IF EXISTS `user`");
+
+	mysqli_query($con, 	"CREATE TABLE IF NOT EXISTS `user` (
+		`username` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+		`password` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+		`firstName` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+		`lastName` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+		`email` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL
+	) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;");
+
+	foreach ($users as $user) {
+		$username = $user['username'];
+		$password = $user['password'];
+		$firstName = $user['firstName'];
+		$lastName = $user['lastName'];
+		$email = $user['email'];
+
+		$salted = "dfjhg584967y98ehg75498y" . $password . "fdsjghiuo54jyi";
+		$hashed = hash('sha512', $salted);
+		$query = "insert into user (username,password,firstName,lastName,email) values ('$username','$hashed','$firstName','$lastName','$email')";
+		//save into db
+		mysqli_query($con, $query);
+	}
+
+	mysqli_query($con, "DROP TABLE IF EXISTS `items`");
+
+	mysqli_query($con, "CREATE TABLE IF NOT EXISTS `items` (
+		`id` int UNSIGNED NOT NULL AUTO_INCREMENT,
+		`title` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+		`description` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+		`category` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+		`price` decimal(10,2) NOT NULL,
+		`user` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+		`created_at` date NOT NULL,
+		PRIMARY KEY (`id`)
+	) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;");
+
+	foreach ($items as $item) {
+		$id = $item['id'];
+		$title = $item['title'];
+		$description = $item['description'];
+		$category = $item['category'];
+		$price = $item['price'];
+		$user = $item['user'];
+		$created_at = $item['created_at'];
+
+		$query = "insert into items (id,title,description,category,price,user,created_at) 
+		values ('$id','$title','$description','$category','$price','$user','$created_at')";
+		//save into db
+		mysqli_query($con, $query);
+	}
+
+	mysqli_query($con, "DROP TABLE IF EXISTS `reviews`");
+
+	mysqli_query($con, "CREATE TABLE IF NOT EXISTS `reviews` (
+		`item_id` int NOT NULL,
+		`id` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+		`rating` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
+		`description` text COLLATE utf8mb4_general_ci NOT NULL
+	) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;");
+
+	foreach ($reviews as $r) {
+		$item_id = $r['item_id'];
+		$id = $r['id'];
+		$rating = $r['rating'];
+		$description = $r['description'];
+
+		$query = "insert into reviews (item_id,id,rating,description) 
+		values ('$item_id','$id','$rating','$description')";
+		//save into db
+		mysqli_query($con, $query);
+	}
+
+	echo '<script>alert("Database Reinitialized");</script>';
 }
+
 ?>
 
 <html lang="en">
@@ -64,6 +148,10 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
 
 			<a href="signup.php">Dont have an account? Sign Up</a><br><br>
 
+		</form>
+
+		<form method="post">
+			<input type="submit" value="Initialize Database"><br><br>
 		</form>
 
 	</div>
